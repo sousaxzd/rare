6a1ee74498c6fe0fba1392e0a530c777ab7fe1d3
Antonio Mayron <mayrobantonio@gmail.com>
2026-02-13 08:40:52 -0300

    fix: remover persistencia do desbloqueio para pedir sempre

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,13 @@
 
 import { CTASection } from '@/components/home/cta'
 import { DiscordBotSection } from '@/components/home/discord-bot-section'
+import { FoundersSection } from '@/components/home/founders-section'
 
 export default function Home() {
   return (
     <main className="flex flex-col pt-24">
       <DiscordBotSection />
+      <FoundersSection />
       <CTASection />
     </main>
   )
